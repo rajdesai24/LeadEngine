f72d96341b5706c63bbc45bb1eff88f53851f69f
raj.py <rajdesai024@gmail.com>
2024-12-20 11:13:38 +0530

    improving code and coding standards

--- lead_engine_app/serializers.py
+++ lead_engine_app/serializers.py
@@ -9,23 +9,16 @@ class MagicBricksLeadSerializer(serializers.Serializer):
     """
     Serializer for 99 Acres lead data.
     """
-    name = serializers.CharField(max_length=255, required=True)
-    email = serializers.EmailField(required=True)
-    phone = serializers.CharField(max_length=15, required=True)
-    source = serializers.CharField(max_length=50, required=True)
-    message = serializers.CharField(max_length=500, required=False, allow_blank=True)
-    property_id = serializers.CharField(max_length=50, required=False, allow_blank=True)
-    query_date = serializers.DateTimeField(required=False, allow_null=True)
+    class Meta:
+        model = Lead
+        fields = ["lead_name", "email_id", "phone", "company_name"]
+
 
 class HousingLeadSerializer(serializers.Serializer):
     """
     Serializer for 99 Acres lead data.
     """
-    name = serializers.CharField(max_length=255, required=True)
-    email = serializers.EmailField(required=True)
-    phone = serializers.CharField(max_length=15, required=True)
-    source = serializers.CharField(max_length=50, required=True)
-    message = serializers.CharField(max_length=500, required=False, allow_blank=True)
-    property_id = serializers.CharField(max_length=50, required=False, allow_blank=True)
-    query_date = serializers.DateTimeField(required=False, allow_null=True)
+    class Meta:
+        model = Lead
+        fields = ["lead_name", "email_id", "phone", "company_name"]
 
